055b3cc38cb81730f9c1d7faf95b83445f59afcf
ninuo.dong <ninuo.dong@tuya.com>
2022-07-30 18:29:38 +0800

    package init

---
+++ README.md
@@ -0,0 +1,3 @@
+# VaporExtension
+
+A description of this package.
---
+++ Sources/VaporExtension/VaporExtension.swift
@@ -0,0 +1,6 @@
+public struct VaporExtension {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
