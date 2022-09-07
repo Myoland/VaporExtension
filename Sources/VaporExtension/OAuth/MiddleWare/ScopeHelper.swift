471ab60b5db64de229228267ee620dcb18680558
ninuo.dong <ninuo.dong@tuya.com>
2022-09-07 13:05:44 +0800

    feat(ScopeHelper): support

--- Sources/VaporExtension/OAuth/MiddleWare/ScopeHelper.swift
+++ Sources/VaporExtension/OAuth/MiddleWare/ScopeHelper.swift
@@ -10,22 +10,24 @@ import Vapor
 
 
 /// 用户 Scope 认证中间件
-struct ScopeHelper: AsyncMiddleware {
+struct ScopeHelper<T: ScopeCarrier>: AsyncMiddleware {
     var scopes: [String]
+    var payload: T.Type
 
-    init(_ scopes: [String]) {
+    init(_ scopes: [String], as payload: T.Type = T.self) {
         self.scopes = scopes
+        self.payload = payload
     }
     
-    init(scope: String) {
-        self.init([scope])
+    init(scope: String, as payload: T.Type = T.self) {
+        self.init([scope], as: payload.self)
     }
     
     func respond(
         to request: Request,
         chainingTo next: AsyncResponder
     ) async throws -> Response {
-        // try request.oauth.assertScopes(self.scopes, as: T.Type)
+        try request.oauth.assertScopes(self.scopes, as: payload.self)
         
         return try await next.respond(to: request)
     }
