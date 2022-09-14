413f65b52ad460a55ca4554c464ba8941fe0d4fe
ninuo.dong <ninuo.dong@tuya.com>
2022-09-14 18:07:18 +0800

    docs(ScopeCarrier): add example

--- Sources/VaporExtension/OAuth/ScopeCarrier.swift
+++ Sources/VaporExtension/OAuth/ScopeCarrier.swift
@@ -8,6 +8,22 @@
 import Vapor
 import JWT
 
+
+/// Example:
+/// ```
+/// public struct User: ScopeCarrier {
+///   public var subject: SubjectClaim
+///   public var expiration: ExpirationClaim
+///   public var scopes: [String]
+///
+///   public func verify(using signer: JWTSigner) throws {
+///       try self.expiration.verifyNotExpired()
+///   }
+/// }
+///
+/// let user = User(...)
+/// try await User.authenticator().authenticate(jwt: user, for: request)
+/// ```
 public protocol ScopeCarrier: JWTPayload, Authenticatable {
     var scopes: [String] { get }
 }
